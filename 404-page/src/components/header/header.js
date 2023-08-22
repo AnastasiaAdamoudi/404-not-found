c796a71bc09abf51da20ad7e905f3dbc800321a5
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-08-22 02:27:25 +0100

    deployed website on github pages

--- 404-page/src/components/header/header.js
+++ 404-page/src/components/header/header.js
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <div className='fixed top-0 w-full bg-white shadow-md z-50 p-8'>
+        <div className='fixed top-0 w-full bg-white z-50 p-8'>
             <h2 className='font-inconsolata text-2xl'>404 NOT FOUND</h2>
         </div>
     )
