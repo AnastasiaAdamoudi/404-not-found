5a83beb94aaaef1aba5a590eb9100990305f18c8
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-08-22 16:46:06 +0100

    trying to fix missing image

--- 404-page/src/components/content/image.js
+++ 404-page/src/components/content/image.js
@@ -1,7 +1,9 @@
+import { scarecrow } from "./Scarecrow.png";
+
 const Image = () => {
     return (
       <div className="flex justify-center">
-        <img src="/Scarecrow.png" alt="404-scarecrow" className="w-1/2 md:w-1/2 mx-auto" />
+        <img src={scarecrow} alt="404-scarecrow" className="w-1/2 md:w-1/2 mx-auto" />
       </div>
     );
   };
